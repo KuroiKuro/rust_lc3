ca1e2cb344c0b33919fefa1a9244f4f43fa1f528
KuroiKuro <ronseahald@hotmail.com>
2023-04-25 15:51:38 +0800

    Refactor registers

--- src/vm/registers.rs
+++ src/vm/registers.rs
@@ -11,7 +11,7 @@ impl Register {
     }
 }
 
-const REGISTER_COUNT: usize = 10;
+const GENERAL_REGISTER_COUNT: usize = 8;
 pub enum RegisterName {
     R0 = 0,
     R1 = 1,
@@ -25,23 +25,74 @@ pub enum RegisterName {
     Cond = 9,
 }
 
+#[derive(Clone, Copy)]
+pub enum ConditionFlag {
+    Pos = 0b001,
+    Zro = 0b010,
+    Neg = 0b100,
+}
+
+impl From<u16> for ConditionFlag {
+    fn from(value: u16) -> Self {
+        match value {
+            0b001 => ConditionFlag::Pos,
+            0b010 => ConditionFlag::Zro,
+            0b100 => ConditionFlag::Neg,
+            _ => panic!("Invalid value"),
+        }
+    }
+}
+
+impl From<ConditionFlag> for u16 {
+    fn from(value: ConditionFlag) -> Self {
+        value as u16
+    }
+}
+
 pub struct Registers {
-    regs: [Register; REGISTER_COUNT],
+    general_regs: [Register; GENERAL_REGISTER_COUNT],
+    program_counter_reg: Register,
+    /// Condition register stores condition flags about most recently executed calcs.
+    /// This allows comparisons, etc
+    condition_reg: Register
 }
 
 impl Registers {
     pub fn new() -> Self {
-        let regs: [Register; REGISTER_COUNT] = [Register(0); REGISTER_COUNT];
-        Self {regs}
+        let general_regs: [Register; GENERAL_REGISTER_COUNT] = [Register(0); GENERAL_REGISTER_COUNT];
+        let program_counter_reg = Register(0);
+        let condition_reg = Register(0);
+        Self {general_regs, program_counter_reg, condition_reg}
     }
 
-    pub fn get_register_value(&self, register: RegisterName) -> u16 {
+    pub fn get_reg_value(&self, register: RegisterName) -> u16 {
         let index = register as usize;
-        self.regs[index].value()
+        self.general_regs[index].value()
     }
 
-    pub fn set_register_value(&mut self, register: RegisterName, value: u16) {
+    pub fn set_reg_value(&mut self, register: RegisterName, value: u16) {
         let index = register as usize;
-        self.regs[index].set(value);
+        self.general_regs[index].set(value);
     }
+
+    pub fn program_counter(&self) -> u16 {
+        self.program_counter_reg.value()
+    }
+
+    pub fn increment_program_counter(&mut self) {
+        let current_val = self.program_counter_reg.value() + 1;
+        self.program_counter_reg.set(current_val);
+    }
+
+    pub fn set_program_counter(&mut self, value: u16) {
+        self.program_counter_reg.set(value);
+    }
+
+    pub fn cond_reg(&self) -> u16 {
+        self.condition_reg.value()
+    }
+
+    pub fn set_cond_reg(&mut self, flag: ConditionFlag) {
+        self.condition_reg.set(flag.into());
+    } 
 }
