78910fa20947ce8424478385619de494905e793a
KuroiKuro <ronseahald@hotmail.com>
2023-04-26 11:03:42 +0800

    Fix sign extension and add test for imm mode

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -53,13 +53,14 @@ impl Lc3Vm {
 
 /// Sign extension of numbers, when the number is smaller than 16 bits
 /// TODO: Research https://en.wikipedia.org/wiki/Two%27s_complement
-fn sign_extend(x: u16, bit_count: u16) -> u16 {
-    let mut x_arg = x;
+fn sign_extend(x: u16, bit_count: u16) -> i16 {
+    let mut x_arg: i16 = x.try_into().unwrap();
 
     // Check if the most significant bit is a 1. If it is then fill in 1s for the
-    // left padding
+    // left padding. We do AND 1 on the number to remove the possibility that the
+    // most significant digits are 1
     if ((x_arg >> (bit_count - 1)) & 1) == 1 {
-        x_arg |= 0xFFFF << bit_count;
+        x_arg |= i16::MAX << bit_count;
     }
     x_arg
 }
@@ -68,22 +69,23 @@ fn add_op(instr: u16, vm: &mut Lc3Vm) {
     // Use bitwise AND to retrieve only the bit that we are interested in
     let dest_reg = (instr >> 9) & 0b111;
     let first_reg = (instr >> 6) & 0b111;
-    let first_val = vm.get_reg_val_by_id(first_reg);
+    let first_val: i16 = vm.get_reg_val_by_id(first_reg).try_into().unwrap();
 
     // Check bit[5], if 0 then register mode else immediate mode
     let register_mode = ((instr >> 5) & 0b1) == 0;
-    let second_val = if register_mode {
+    let second_val: i16 = if register_mode {
         // Get the 2nd register for register mode
         let second_reg = instr & 0b111;
-        vm.get_reg_val_by_id(second_reg)
+        vm.get_reg_val_by_id(second_reg).try_into().unwrap()
     } else {
         let imm_val = instr & 0b11111;
         // Imm val is 5 bits, according to the spec
         sign_extend(imm_val, 5)
+        // imm_val
     };
 
     let added = first_val + second_val;
-    vm.set_reg_val_by_id(dest_reg, added);
+    vm.set_reg_val_by_id(dest_reg, added.try_into().unwrap());
     let cond_flag = match added.cmp(&0) {
         Ordering::Equal => ConditionFlag::Zro,
         Ordering::Greater => ConditionFlag::Pos,
@@ -98,7 +100,7 @@ mod tests {
 
     #[test]
     #[allow(clippy::unusual_byte_groupings)]
-    fn test_add_op() {
+    fn test_add_op_register_mode() {
         let mut vm = Lc3Vm::new();
         // ADD R2, R3, R4
         let instr: u16 = 0b0001_001_010_000_011;
@@ -111,5 +113,25 @@ mod tests {
         add_op(instr, &mut vm);
         let added_val = vm.get_reg_val_by_id(1);
         assert_eq!(added_val, first + second);
+    }
+
+    #[test]
+    #[allow(clippy::unusual_byte_groupings)]
+    fn test_add_op_imm_mode() {
+        let mut vm = Lc3Vm::new();
+        // ADD R0, R1, 3
+        let val = 3;
+        let instr: u16 = 0b0001_000_001_1_00011;
+
+        // Test with adding to 0
+        add_op(instr, &mut vm);
+        let added_val = vm.get_reg_val_by_id(0);
+        assert_eq!(added_val, val);
+
+        // ADD R1, R0, -1
+        let instr: u16 = 0b0001_001_000_1_11111;
+        add_op(instr, &mut vm);
+        let added_val = vm.get_reg_val_by_id(1);
+        assert_eq!(added_val, 3 - 1);
     }
 }
