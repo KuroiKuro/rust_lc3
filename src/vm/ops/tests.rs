5a39d8b1c44f0740fcd6390f20bfd83751fed99b
KuroiKuro <ronseahald@hotmail.com>
2023-04-26 14:34:35 +0800

    Create test for ldi op

--- src/vm/ops/tests.rs
+++ src/vm/ops/tests.rs
@@ -36,3 +36,19 @@ fn test_add_op_imm_mode() {
     let added_val = vm.get_reg_val_by_id(1);
     assert_eq!(added_val, 3 - 1);
 }
+
+#[test]
+#[allow(clippy::unusual_byte_groupings)]
+fn test_ldi_op() {
+    let data: u16 = 1234;
+    let desired_address: u16 = 0x3050;
+    let offset = desired_address - Lc3Vm::DEFAULT_PC_START;
+    // LDI R3, ${offset}
+    let instr: u16 = 0b1010_010_001010000;
+
+    let mut vm = Lc3Vm::new();
+    vm.memory.write(desired_address, data);
+    vm.ldi_op(instr);
+    let reg_val = vm.get_reg_val_by_id(2);
+    assert_eq!(reg_val, data);
+}
