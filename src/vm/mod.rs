79658d364a2ac93ecb3922595d68f387d354e724
KuroiKuro <ronseahald@hotmail.com>
2023-05-10 11:51:02 +0800

    Implement function to load program data into VM

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -2,8 +2,10 @@ mod memory;
 mod ops;
 mod registers;
 mod trap_vecs;
+#[cfg(test)]
+mod tests;
 
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fs::File, io::{Read, BufReader}, slice::Chunks, path::Path};
 
 use memory::Memory;
 use registers::Registers;
@@ -26,6 +28,55 @@ impl Lc3Vm {
         let mut vm = Self { registers, memory, running };
         vm.registers.set_program_counter(Self::DEFAULT_PC_START);
         vm
+    }
+
+    /// Load a compiled LC3 program for execution.
+    /// 
+    /// A given LC3 program will have its first 16 bits set to the memory address
+    /// where the start of the program instructions should be loaded to. Subsequent
+    /// bytes are then the program instructions
+    pub fn load_program(&mut self, file_path: &Path) {
+        let mut program_file = File::open(file_path).unwrap();
+        let mut file_contents: Vec<u8> = Vec::new();
+        program_file.read_to_end(&mut file_contents).unwrap();
+
+        // Read the origin first
+        let mut chunked = file_contents.as_slice()
+            .chunks(2);
+
+        let chunk = chunked.next().unwrap();
+        let origin = Self::read_u16(chunk);
+        let file_len = program_file.metadata().unwrap().len();
+        if let Err(e) = Self::validate_file_len(file_len, origin) {
+            panic!("The length of the file ({}) will be too large to fit into VM memory!", e);
+        }
+
+        let mut current_address = origin;
+        for chunk in chunked {
+            let mem_data = Self::read_u16(chunk);
+            self.memory.write(current_address, mem_data);
+            current_address += 1;
+        }
+    }
+
+    /// Reads two bytes from file data that has been converted into a `Chunks<u8>`,
+    /// and then parse it into a `u16` and return it
+    fn read_u16(program_data_slice: &[u8]) -> u16 {
+        // Program file is in little endian, so convert it to big endian, which
+        // is required in LC3
+        let buf: [u8; 2] = [program_data_slice[0], program_data_slice[1]];
+        u16::from_le_bytes(buf)
+    }
+
+    /// Validate that the program file is small enough to fit in the memory of
+    /// the LC3 VM. The `file_len` argument should be the size of the files generated
+    /// by the file's `std::fs::Metadata::len()` function
+    fn validate_file_len(file_len: u64, origin: u16) -> Result<(), u64> {
+        if file_len > (u16::MAX - origin) as u64 {
+            Err(file_len)
+        } else {
+            Ok(())
+        }
     }
 
     pub fn run(&mut self) {
---
+++ src/vm/tests.rs
@@ -0,0 +1,68 @@
+use std::io::Write;
+
+use super::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn test_load_program() {
+    let mut vm = Lc3Vm::new();
+    let start_address = 0x3001;
+    // Create a tempfile to contain the testing program logic
+    let mut temp_file = NamedTempFile::new().unwrap();
+    let program_data: Vec<u16> = vec![
+        start_address,
+        0x39ab,
+        0x341f,
+        0x3333,
+        0x31cb,
+    ];
+
+    let write_data = program_data.iter()
+        .flat_map(|prog_data_piece| prog_data_piece.to_le_bytes())
+        .collect::<Vec<u8>>();
+    temp_file.write_all(&write_data).unwrap();
+
+    let temp_file_path = temp_file.path();
+    vm.load_program(temp_file_path);
+
+    // Check that 0x3000 is empty, since we configured the start to be another
+    // address
+    let check_addr = vm.memory.read(0x3000);
+    assert_eq!(check_addr, 0);
+
+    let mut current_address = start_address;
+    // Remove start address from data to check
+    for (i, data) in program_data.iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+        let check_data = vm.memory.read(current_address);
+        assert_eq!(check_data, *data);
+        current_address += 1;
+    }
+    // Check the address after is empty, as nothing should be written after the program
+    // is written
+    let after_data = vm.memory.read(current_address);
+    assert_eq!(after_data, 0);
+}
+
+#[test]
+fn test_read_u16() {
+    let le_bytes: [u8; 2] = [0x2b, 0x2e];
+    let be_u16 = Lc3Vm::read_u16(&le_bytes);
+    assert_eq!(be_u16, 0x2e2b);
+}
+
+#[test]
+fn test_validate_file_len() {
+    let invalid_file_len: u64 = 65536;
+    let valid_file_len: u64 = 2000;
+    let origin = 0x3000;
+    
+    let invalid_check = Lc3Vm::validate_file_len(invalid_file_len, origin);
+    let valid_check = Lc3Vm::validate_file_len(valid_file_len, origin);
+    assert!(valid_check.is_ok());
+    assert!(invalid_check.is_err());
+    let err = invalid_check.unwrap_err();
+    assert_eq!(err, invalid_file_len);
+}
