7d2c21f88a58aef5edb13940c7b1e0cd4eb833b4
KuroiKuro <ronseahald@hotmail.com>
2023-05-11 14:54:13 +0800

    Save in kbdr

--- src/vm/memory/mod.rs
+++ src/vm/memory/mod.rs
@@ -6,26 +6,28 @@ use std::io::Read;
 /// Maximum size a `u16` can hold
 const MEMORY_MAX: usize = 1 << 16;
 
+const DEVICE_REGISTER_COUNT: usize = 5;
+
 /// Enum representing the memory mapped device registers specified by LC3. Reading
 /// and writing to these registers involved a memory read/write operation to the
 /// specific memory address specified by each of these registers.
 pub enum DeviceRegister {
     /// Keyboard status register. The ready bit (bit [15]) indicates if the keyboard has
     /// received a new character. Mapped to `0xFE00`.
-    Kbsr,
+    Kbsr = 0,
     /// Keyboard data register. Bits [7:0] contain the last character typed on the
     /// keyboard. Mapped to `0xFE02`.
-    Kbdr,
+    Kbdr = 1,
     /// Display status register. The ready bit (bit [15]) indicates if the display
     /// device is ready to receive another character to print on the screen.
     /// Mapped to `0xFE04`.
-    Dsr,
+    Dsr = 2,
     /// Display data register. A character written in the low byte of this register
     /// will be displayed on the screen. Mapped to `0xFE06`.
-    Ddr,
+    Ddr = 3,
     /// Machine control register. Bit [15] is the clock enable bit. When cleared,
     /// instruction processing stops. Mapped to `0XFFFE`.
-    Mcr,
+    Mcr = 4,
 }
 
 impl DeviceRegister {
@@ -70,14 +72,26 @@ impl MemorySlice {
     }
 }
 
+/// A struct containing the values of certain device registers where the value has to be saved
+struct MmapRegisters {
+    kbdr: MemorySlice,
+}
+
+impl MmapRegisters {
+    fn new() -> Self { Self { kbdr: MemorySlice(0) } }
+}
+
+
 pub struct Memory {
     mem_arr: [MemorySlice; MEMORY_MAX],
+    mmap_registers: MmapRegisters,
 }
 
 impl Memory {
     pub fn new() -> Self {
         let mem_arr: [MemorySlice; MEMORY_MAX] = [MemorySlice(0); MEMORY_MAX];
-        Self { mem_arr }
+        let mmap_registers = MmapRegisters::new();
+        Self { mem_arr, mmap_registers }
     }
 
     /// Reads the value at the given memory address
@@ -100,13 +114,20 @@ impl Memory {
         self.mem_arr[address].write(value);
     }
 
-    fn read_kbsr(&self, input_reader: &mut impl Read) -> u16 {
+    fn read_kbsr(&mut self, input_reader: &mut impl Read) -> u16 {
         // We need to check if the input has any new character
         let mut buf: [u8; 1] = [0];
         // If read_exact returns Err, then it means there's nothing
         match input_reader.read_exact(&mut buf) {
-            Ok(_) => 0x8000,
             Err(_) => 0,
+            Ok(_) => {
+                // If the read was successfuly, then we must save the character we
+                // read to KBDR because it's no longer available in the input.
+                // We reuse the actual memory address reserved for Kbdr since it's
+                // reserved and nothing else can read it (normally)
+                self.mmap_registers.kbdr.write(buf[0] as u16);
+                0x8000
+            },
         }
     }
 }
--- src/vm/memory/tests.rs
+++ src/vm/memory/tests.rs
@@ -1,13 +1,24 @@
+use ascii::AsciiChar;
+
 use crate::vm::Lc3Vm;
 
 #[test]
 fn test_read_kbsr() {
-    let vm = Lc3Vm::new();
+    let mut vm = Lc3Vm::new();
     let mut input = "y".as_bytes();
     let value = vm.memory.read_kbsr(&mut input);
     assert_eq!(value, 0x8000);
 
+    // Test that kbdr has been updated
+    let kbdr_val = vm.memory.mmap_registers.kbdr.read();
+    let expected_val = AsciiChar::new('y') as u16;
+    assert_eq!(kbdr_val, expected_val);
+
     let mut input = "".as_bytes();
     let value = vm.memory.read_kbsr(&mut input);
     assert_eq!(value, 0);
+
+    // Kbdr value should still be there
+    let kbdr_val = vm.memory.mmap_registers.kbdr.read();
+    assert_eq!(kbdr_val, expected_val);
 }
